00a40108db86d85cd913ccd01ccd11f2356dad5d
brandonmok <brandonmok88@yahoo.com>
2019-02-16 11:03:53 -0500

    Question data source

---
+++ js/questionData.js
@@ -0,0 +1,17 @@
+/**
+* Brandon Mok
+* Questions data source
+*/
+
+function questionData(){
+    var qData = new Object();
+    qData["init"]=["Choose a vehicle"];
+    qData["car"]=["Choose a model"];
+    qData["sedan"]=["Choose a transmission"];
+    qData["hatchback"]=["Choose a seat material"];
+
+    qData["truck"]=["Choose a truck type"];
+    qData["pickup"]=["Choose a fuel type"];
+    qData["gas"]=["How many seats?"];
+    qData["diseal"]=["What size mirrors?"];
+}
