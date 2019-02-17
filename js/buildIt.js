f6e0eb13068939ec3bb913cd15a6a4958cf22069
brandonmok <brandonmok88@yahoo.com>
2019-02-17 17:52:20 -0500

    ReWorked BuildIT

--- js/buildIt.js
+++ js/buildIt.js
@@ -8,56 +8,69 @@ var choices = []; // global array to hold choices
 
 
 function buildIt(dom){
-    // value of chosen choice
-    var userClicked = dom.value; 
-
-    // Add user choices to array
-    choices.push(userClicked);
-
 
     var menu;  // menu
     var options; // options
     var question; // questions 
-    
-    // -- Select -- preOption
-    // var preOption = document.createElement('option');
-    // preOption.appendChild(document.createTextNode("-- Select --"));
+
+    var hold; // hold used to hold dom
+    var holdQ;  // Quetion hold that does same as hold, but for Questions data
+
+    // Array used to hold entered choices
+    choices.push(dom.value);
 
 
-    // check that there's a value
-    if(userClicked != undefined){
+    // First check for outdated browsers
+    if(!document.getElementById){
+        window.location = "http://outdatedbrowser.com";
+    }else{
+
+
+        // Check for the initial 'init'
+        if(dom == "init"){
+            hold = data["init"];    // get the data for using the key
+            holdQ = qData["init"];  // get the question data using the key
+        }else{
+            hold = data[dom.value]; // Use the value, not the DOM obj for data
+            holdQ = qData[dom.value];   // Use the value, not the DOM obj to get the question data
+        }
+
 
         // Check if user chose the '-- Select --' option
-        if(userClicked === "-- Select --"){
+        if(dom.value == "-- Select --"){
             return; // return nothing
         }
-        if(data[userClicked] == undefined){ // No more items!
 
+
+        // Check if there aren't any items/data
+        if(hold == undefined){ 
             // Header
             var finalDispHeader = document.createElement('h1');
             finalDispHeader.appendChild(document.createTextNode("Your selections:"));
             $('finalDisplay').appendChild(finalDispHeader);
 
 
-            // var used to create 'p' tags to hold choices
-            var selectedChoices; 
-
-
             // Display choices - Place in displayDiv
             // NOTE: i is set to 1 initially to skip the 'init' from showing
             for(var i = 1; i < choices.length; i++){
-                selectedChoices = document.createElement('p');
+                var selectedChoices = document.createElement('p');
                 selectedChoices.appendChild(document.createTextNode(choices[i]));
                 $('finalDisplay').appendChild(selectedChoices);
             }
 
+
             buildFinalForm(); // builds the final form for gathering user information
         }
-        else{ 
+
+        
+        // Check if there ARE options available 
+        if(hold != undefined){
+
             // Show Question first
-            question = document.createTextNode(qData[userClicked][0]);
+            question = document.createTextNode(holdQ[0]);
             $('forms').appendChild(question);
 
+            
             // Menu creation
             menu = document.createElement('select');
             menu.setAttribute('onchange','buildIt(this);');
@@ -65,13 +78,19 @@ function buildIt(dom){
 
 
             // Loop that makes options, loads option data, and puts it in menu
-            for(var i = 0; i < data[userClicked].length; i++){
+            for(var i = 0; i < hold.length; i++){
                 options = document.createElement('option');
-                options.setAttribute('name', data[userClicked][i]);
-                options.setAttribute('value', data[userClicked][i]);
-                options.appendChild(document.createTextNode(data[userClicked][i]));
+                options.setAttribute('name', hold[i]);
+                options.setAttribute('value', hold[i]);
+                options.appendChild(document.createTextNode(hold[i]));
                 menu.appendChild(options);
             }
-        }
+        }      
     }
+}
+
+
+
+function $(id){
+    return document.getElementById(id);
 }
