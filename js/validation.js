eb1a4f384139a6a43abf70501b3d0d9071702baa
brandonmok <brandonmok88@yahoo.com>
2019-02-17 17:07:18 -0500

    Changed validation

--- js/validation.js
+++ js/validation.js
@@ -6,15 +6,9 @@
 function validation(){
 
     // Check for empty fields
-    if($('fName').value == ''){
+    if($('fName').value == '' || $('lName').value == '' || $('email').value == ''){
         $('fName').style.backgroundColor = "#EF4648";
-        return false;
-    }
-    if($('lName').value == ''){
         $('lName').style.backgroundColor = "#EF4648";
-        return false;
-    }
-    if($('email').value == ''){
         $('email').style.backgroundColor = "#EF4648";
         return false;
     }
