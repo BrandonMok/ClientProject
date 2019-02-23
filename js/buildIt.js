391456e173f3e8f59c0c3968f91bade127f47307
brandonmok <brandonmok88@yahoo.com>
2019-02-22 21:28:28 -0500

    Removal of form and selected choices when reset

--- js/buildIt.js
+++ js/buildIt.js
@@ -13,8 +13,8 @@ function buildIt(dom){
     var holdQ;  // Quetion hold that does same as hold, but for Questions data
 
     // Array used to hold entered choices
-    choices.push(dom.value);
-
+    //choices.push(dom.value);
+    choices.push(dom);
 
 
     // First check for outdated browsers
@@ -33,8 +33,8 @@ function buildIt(dom){
 
 
         // Check if user chose the '-- Select --' option
-        // ACTS as a precaution
         if(dom.value == "-- Select --"){
+            choices.pop(); // Remove option from being tracked
             return; // return nothing
         }
 
@@ -54,16 +54,17 @@ function buildIt(dom){
 
             // Display choices - Place in displayDiv
             // NOTE: i is set to 1 initially to skip the 'init' from showing
-            for(var i = 1; i < choices.length; i++){
+            for(var i = 1, len = choices.length; i < len; i++){
                 var selectedChoices = document.createElement('p');
-                selectedChoices.appendChild(document.createTextNode(choices[i]));
+                selectedChoices.appendChild(document.createTextNode(choices[i].value));
                 $('contentDiv').appendChild(selectedChoices);
             }
 
             buildFinalForm(); // builds the final form for gathering user information
         }
-        else{// Case there ARE options available and then make the Select menu and options
-
+        else{
+            // Case there are available choices
+           
             /**
              * Removes choices depending on previous
              * Only works if an option menu - can't be the initial 'init' bc it's the first form
@@ -72,20 +73,29 @@ function buildIt(dom){
                 // Case that user starts over 
                 // Checks to see if that menu is the last, if not remove all others
                 while(dom != dom.parentNode.lastChild){
-                    dom.parentNode.removeChild(dom.parentNode.lastChild);
+                    dom.parentNode.removeChild(dom.parentNode.lastChild);  
+
+                    // Also delete the form 
+                    if($('myFinalForm')){
+                        $('myFinalForm').remove();
+                    }  
+                    // Also delete the dynamic div that display selected choices
+                    if($('contentDiv')){
+                        $('contentDiv').remove();
+                    }
                 }
             }
 
 
             // Container div to hold question and menu
-            var questionAnswerCont = document.createElement('div');
-            questionAnswerCont.setAttribute('id','formPackage');
-            $('forms').appendChild(questionAnswerCont);
+            var selectMenuDIV = document.createElement('div');
+            selectMenuDIV.setAttribute('id','menuDIV');
+            $('forms').appendChild(selectMenuDIV);
 
             // Show Question first
             var question = document.createElement('h3');
             question.appendChild(document.createTextNode((holdQ[0])));
-            $('formPackage').appendChild(question);
+            $('menuDIV').appendChild(question);
 
             // Menu creation
             var menu = document.createElement('select');
@@ -94,16 +104,16 @@ function buildIt(dom){
             }else{
                 menu.setAttribute('onchange','buildIt(this);');
             }
-            $('formPackage').appendChild(menu);
+            $('menuDIV').appendChild(menu);
 
 
             // Loop that makes options, loads option data, and puts it in menu
-            for(var i = 0; i < hold.length; i++){
+            for(var i = 0, len = hold.length; i < len; i++){
                 var options = document.createElement('option');
                 options.setAttribute('name', hold[i]);
                 options.setAttribute('value', hold[i]);
                 options.appendChild(document.createTextNode(hold[i]));
-                menu.appendChild(options);
+                menu.appendChild(options);  // append options to the menu
             }
         }      
     }
@@ -111,6 +121,9 @@ function buildIt(dom){
 
 
 
+/**
+ * Shortcut for getting elements by id
+ */
 function $(id){
     return document.getElementById(id);
 }
